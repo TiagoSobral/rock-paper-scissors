96ba0d8e36d0964f326658d6c2c8a43249a5a44e
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2024-11-15 22:15:07 +0000

    (fix) clean code and add comments for readability.

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,4 @@
 
-
-/* 
-Planing:
-- the program has no interface
-- the only interaction will be the user choosing ROCK, PAPER or SCISSORS.
-- the user will have to input the data, (a string into the console).
-- the desired output will be to compare the input from the user versus the output from the computer and compare to see who wins.
-- the game will be a best of 3 (possibly)
-
-
-- the computer chooses randomly ROCK, PAPER or SCISSORS.
-- the user chooses ROCK PAPER or SCISSORS.
-- what did the computer choose?
-- what did the user choose?
-- ROCK beats SCISSORS; SCISSORS beats PAPER; PAPER beats ROCK;
-
-
-
-Pseudocode:
-- when the user inputs a string : "Rock", "PAPER" or "Scissors";
-- the computer should randomize and output one of the same 3 choices.
-- if the user chooses ROCK and computer SCISSORS print (computer looses)
-- if the user chooses SCISSORS and computer chooses PAPER print (computer looses) 
-- if the user chooses PAPER and computer ROCK print (computer looses.)
-- if the user chooses the same as the computer print (its a tie! try again!)
-- else print (user looses! Play again!)
-*/
-
-
-
 /*create a function named getComputerChoice 
 (used a numer to attach to a specific string in order to return the string
 when computer randomizes the number that outputs.*/
@@ -61,15 +31,15 @@ function getHumanChoice() {
     }
 }
 
+/* create 2 new variable names humanScore & computerScore
+in the global scope */
+
 let humanScore = 0;
 let computerScore = 0;
 
+
+
 function playGame(human,computer) {
-
-    /* create 2 new variable names humanScore & computerScore
-in the global scope */
-
-
 
 /* create a new function named playRound */
 
@@ -110,15 +80,21 @@ playRound(humanSelection,computerSelection);
 // Score message with increment in it
 console.log(`Score: User ${humanScore} - ${computerScore} Computer`);
 
+
+// emply console log, to improve readability rounds
 console.log("");
 
 }
+
+/* Loop to play 5 rounds of the game */
 
 let i = playGame();
 
 for (i = 1; i < 5; i++) {
     playGame();
 }
+
+/* if statement to print win messsage at the end of the 5th round*/
 
 if (humanScore > computerScore) {
     console.log("The User Wins the Game!");
@@ -131,16 +107,6 @@ else {
 }
 
 
-
-/* 
-- store human score
-- store computer score
-
-- return human score
-- return computer score
-
-- add human score to new one
-- add computer score to new one */
 
 
 
